d859eef7d3744d93956ed6128b271afa2d80c0b0
Pranali <pranalilambat00@gmail.com>
2022-02-19 16:47:41 +0530

    UC_4_POSITION100.cs

---
+++ UC_4_POSITION100.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake_And_Ladder
+{
+    class UC_4_POSITION100
+    {
+      
+        public const int maxPosition = 100;
+        public const int samePosition = 0;
+        public const int forward = 1;
+        public const int backward = 2;
+        public static void repeatTillPosition100()
+        {
+            Console.WriteLine("-------------------------------------");             //For separation
+
+            //Variables
+            int currentPosition = 1;
+
+            //Computation
+            while (currentPosition <= maxPosition)
+            {
+                Random random = new Random();
+                int dice = random.Next(1, 7);
+                Console.WriteLine("You got " + dice + " in dice roll.");
+
+                Random random1 = new Random();
+                int options = random1.Next(0, 3);
+
+                if (options == samePosition)
+                {
+                    Console.WriteLine("You Got No Option Stay in the Same Place.");
+                }
+                else if (options == forward)
+                {
+                    currentPosition = currentPosition + dice;
+                    Console.WriteLine("You Got Ladder Your Moving Forward by " + dice + " numbers.");
+                }
+                else if (options == backward)
+                {
+                    currentPosition = currentPosition - dice;
+                    Console.WriteLine("You Got Snakebite Your Moving Backward by " + dice + " numbers.");
+                }
+                if (currentPosition >= 100)
+                {
+                    Console.WriteLine("Congratulations You Won the Game Your at 100 position :)");
+                    break;
+                }
+                else if (currentPosition <= 0)
+                {
+                    Console.WriteLine("You lost the Game Your at 0 position. Restarting the Game :(");
+                    currentPosition = currentPosition - currentPosition;
+                }
+                Console.WriteLine("Your current Position is: " + currentPosition);
+                Console.WriteLine("-------------------------------------");                 //For separation
+            }
+        }
+    }
+
+    }
+
