5c9306cd012413a1e11387405d5a28c9f1d518d8
Pranali <pranalilambat00@gmail.com>
2022-02-19 16:28:23 +0530

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Snake_And_Ladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the snake_And_ladder Program");
+            //UC_2_diceRoll.diceRoll();
+            //UC_3_Check_For_Options.Check_For_3Option();
+            //UC_4_POSITION100.repeatTillPosition100();
+            //UC_5EXACT100.exactWinningPosition100();
+            //UC_6_numberOfTimeDiceRolled.numberOfTimeDiceRolled();
+            UC_7.twoPlayers();
+        }
+    }
+}
