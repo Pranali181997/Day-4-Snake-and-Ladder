44249aa626239784919f211e0e8d1cbea53cb2a9
Pranali <pranalilambat00@gmail.com>
2022-02-19 16:42:47 +0530

    UC_2_ROLLSTHEDIE

---
+++ UC2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake_And_Ladder
+{
+    class UC_2_diceRoll
+    {
+        public static void diceRoll() 
+        {
+            Random random = new Random();
+            int num = random.Next(0, 7);
+            Console.WriteLine("You Got " + num);
+        }
+    }
+}
